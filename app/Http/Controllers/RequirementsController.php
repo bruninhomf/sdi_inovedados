2f4be3b87f6c66f1ecd6f4831cae87b3f6cbee33
bruninhomf <bruninhomf@msn.com>
2020-11-25 19:22:53 -0300

    Criando controller de requisitos

--- app/Http/Controllers/RequirementsController.php
+++ app/Http/Controllers/RequirementsController.php
@@ -2,22 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Requirements;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class RequirementsController extends Controller
 {
-    public function verify()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        // echo "<p>teste</p>";
-        
-        // if (Auth::check()) {
-        //     $user = Auth::user();
-        //     $nome = $user->name;
-        //     echo "<p>" . $nome . "</p>";
-        // }
-        // else {
-        //    echo '<h4> Você não está logado </h4>';
-        // }
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Requirements  $requirements
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Requirements $requirements)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Requirements  $requirements
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Requirements $requirements)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Requirements  $requirements
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Requirements $requirements)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Requirements  $requirements
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Requirements $requirements)
+    {
+        //
     }
 }
