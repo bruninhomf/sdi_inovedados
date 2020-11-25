3fe6465e875bc1ca35cdd2de2a6941cc49681d25
bruninhomf <bruninhomf@msn.com>
2020-11-25 15:26:02 -0300

    Personalizando a pagina de exibição de PDF

--- resources/views/pages/storage-pdf-proposal.blade.php
+++ resources/views/pages/storage-pdf-proposal.blade.php
@@ -1,83 +1,152 @@
-{{-- layout --}}
-@extends('layouts.contentLayoutMaster')
-
-{{-- page title --}}
-@section('title','Users View')
-
-{{-- page style --}}
-@section('page-style')
-<link rel="stylesheet" type="text/css" href="{{asset('css/pages/page-users.css')}}">
-@endsection
-
-{{-- page content  --}}
-@section('content')
-<div class="section users-view">
-  <!-- users view media object start -->
-  <div class="card-panel">
-    <div class="row">
-      <div class="col s12 m7">
-        <div class="display-flex media">
-          <div class="media-body pl-2">
-            <h5 class="media-heading pt-1">
-              <span class="users-view-">PDF</span>
-            </h5>
-          </div>
+<html>
+<head>
+<title>Page Title</title>
+<style>
+  
+</style>
+</head>
+<body>
+<div>
+  <div>
+    <div>
+      <div>
+        <div style="height: 50px; padding-top: 120px; padding-bottom: 150px; text-align: center">
+          <img src="images/logo/inovedados.jpg" alt="Inove Dados" width="250px">
         </div>
-      </div>
-      <div class="col s12 m5 quick-action-btns display-flex justify-content-end align-items-center pt-2" width="500">
-        <a href="/armazenamento" class="btn-small btn-light-indigo">
-          <i class="material-icons mb-5">arrow_back</i>
-          Voltar
-        </a>
-        <a href="/armazenamento/imprimir" class="btn-small btn-waves-light">
-          <i class="material-icons mb-3">print</i>
-          Imprimir
-        </a>
-        <a href="/armazenamento/editar/" class="btn-small indigo">
-          <i class="material-icons mb-5">edit</i>
-          Editar
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-content">
-      <div class="row">
-        <div class="col s12">
-          <table class="striped text-nowrap">
+        <table style="border-spacing: 0; margin: 10px 0px">
+          <tr>
+            <td colspan="2" style="background-color: #be575e; height: 57px;"></td>
+          </tr>
+          <tr>
+            <td style="background-color: #be575e; height: 280px;"></td>
+              <td rowspan="2" style="background-color: #891629; color: white; width: 600px; padding: 0px">
+                <h1 style="font-size: 50px; padding: 0px 0px 0px 50px;">PROPOSTA</h1>
+                <h1 style="font-size: 90px; padding: 0px 0px 0px 50px; font-family: Arial, Helvetica, sans-serif; margin: 0px">TÉCNICA</h1>
+              <div>
+                <h4 style="">Servidor de <br>armazenamento</h4>
+              </div>
+              <div style="background-color: white; width: 200px; height: 10px;">@</div>
+              <div style="text-align: right">
+                <div>contato@inovedados.com.br <span><i>email</i></span></div>
+                <div>+55 (31) 4042-2999 <span><i>phone</i></span></div>
+                <div>Rua dos Guajajaras, 910, Sala 1619 <span><i>place</i></span></div>
+                <div>inovedados <span><i>phone</i></span></div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td style="background-color: #262b2c; color: white; font-size: 50px; height: 230px;">2020</td>
+          </tr>
+        </table>
+        <div style="padding-top: 20px">          
+          <img width="100%" src="images/table_storage.png" alt=""> 
+          <table style="width: 100%">
             <tbody>
-              <tr>
+              <tr style="background-color: #f6f6f6;">
                 <td>Espaço em disco:</td>
-                <td></td>
+                <td >{{ $storageproposal->diskspace }}</td>
               </tr>
-              <tr>
+              <tr style="background-color: #dbdcdd">
                 <td>Tráfego de dados:</td>
-                <td></td>
+                <td>{{ $storageproposal->traffic }}</td>
               </tr>
-              <tr>
+              <tr style="background-color: #f6f6f6">
                 <td>Conexões simultâneas:</td>
-                <td></td>
+                <td>{{ $storageproposal->connections }}</td>
               </tr>
-              <tr>
+              <tr style="background-color: #dbdcdd">
                 <td>Contas de FTP:</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Valor:</td>
-                <td></td>
+                <td>{{ $storageproposal->accounts }}</td>
               </tr>
             </tbody>
           </table>
         </div>
+
+        <div style="padding-left: 20px">
+          <ul>
+            <li>
+              FTP
+            </li>
+            <li>
+              FTPS
+            </li>
+            <li>
+              SFTP
+            </li>
+            <li>
+              SCP
+            </li>
+            <li>
+              FSamba/CIFSTP
+            </li>
+            <li>
+              BorgBackup
+            </li>
+            <li>
+              rsync via SSH
+            </li>
+            <li>
+              HTTPS
+            </li>
+            <li>
+              WebDAV
+            </li>
+            <li>
+              Useable as network drive
+            </li>
+          </ul>
+        </div>
+
+        <div style="padding: 25px">
+          <p>Localização: Data Center Park Falkenstein, Alemanha.</p>
+          <div >
+            <h6>
+              Valor: R$ 3.804,00 (três mil, oitocentos e quatro reais) mensais
+              <br>
+              Validade da proposta: 15 (quinze) dias
+            </h6>
+          </div>
+          <div>
+            <p>
+              Orientações gerais:
+            </p>
+            <ul>
+              <li style="padding-top: 10px">
+                O procedimento de armazenamento de Backup não é garantido e o servidor de armazenamento não deve
+                ser utilizado como única forma de Backup. O CLIENTE deve concordar em manter em local seguro o seu
+                próprio Backup.
+              </li>
+              <li style="padding-top: 15px">
+                Nosso suporte não inclui alterações nos dados armazenados. É válido apenas para o correto funcionamento
+                do servidor de armazenamento, dúvidas e orientações gerais.
+              </li>
+            </ul>
+          </div>
+          <table style="padding: 90px 0px">
+            <tr>
+              <td style="padding-left: -20px; text-align: left;">
+                <img src="images/logo/logo_inove.png" alt="Inove Dados" style="width: 120px">
+              </td>
+              <td style="padding-left: 280px; text-align: right; white-space: nowrap;">
+                <ul>
+                  <li>contato@inovedados.com.br<i >email</i></li>
+                  <li>+55 (31) 4042-2999 <span><i >phone</i></span></li>
+                  <li>Rua dos Guajajaras, 910, Sala 1619 <span><i >place</i></span></li>
+                  <li>inovedados <span><i class="material-icons red-text">phone</i></span></li>
+                </ul>
+              </td>
+            </tr>
+          </table>          
+          <div >
+            <div style="background-color: #891629; width: 100%; height: 10px;"></div>
+          </div>
+        </div>
       </div>
-      <!-- </div> -->
     </div>
   </div>
 </div>
-<!-- users view ends -->
-@endsection
 
-{{-- page script --}}
-@section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
-@endsection
+</body>
+</html>
+
+
