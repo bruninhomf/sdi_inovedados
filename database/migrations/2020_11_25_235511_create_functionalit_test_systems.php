316995ea8cae63bcd1a8f505faa80754c72acf86
bruninhomf <bruninhomf@msn.com>
2020-11-26 00:48:32 -0300

    Adicionando a migration de funcionalidade de testes

---
+++ database/migrations/2020_11_25_235511_create_functionalit_test_systems.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFunctionalitTestSystems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('functionalit_test_systems', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('functionalit_test_systems');
+    }
+}
