8e0c6bf11a24d7e579edeea9b87733626ebaa4c0
bruninhomf <bruninhomf@msn.com>
2020-12-01 11:26:24 -0300

    Montando layout da pagina de cruds

---
+++ resources/views/pages/cruds-test.blade.php
@@ -0,0 +1,83 @@
+{{-- layout --}}
+@extends('layouts.contentLayoutMaster')
+
+{{-- page title --}}
+@section('title','Proposta - Servidor Virtual')
+
+{{-- vendors styles --}}
+@section('vendor-style')
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/css/jquery.dataTables.min.css')}}">
+<link rel="stylesheet" type="text/css"
+  href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
+@endsection
+
+{{-- page styles --}}
+@section('page-style')
+<link rel="stylesheet" type="text/css" href="{{asset('css/pages/page-users.css')}}">
+@endsection
+
+{{-- page content --}}
+@section('content')
+<!-- users list start -->
+<section class="users-list-wrapper section">
+  
+  <div class="users-list-table">
+    <div class="card">
+      <div class="card-content">
+        <div class="row">
+            <div class="col s6">
+                <h4 h4 class="card-title indigo-text pb-5"><strong>Testes - Cruds</strong></h4>
+            </div>
+            <div class="col s6 right-align">
+                <a href="virtual/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
+            </div>
+        </div>
+        <!-- datatable start -->
+        <div class="responsive-table">
+          <table id="users-list-datatable" class="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>CPU</th>
+                <th>Memoria</th>
+                <th>Sistema Operacional</th>
+                <th>Valor</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($crudstestsystems as $key => $crudstestsystem)
+                <tr>
+                  <td>{{ $crudstestsystem->id }}</td>
+                  <td>{{ $crudstestsystem->cpu }}</td>
+                  <td>{{ $crudstestsystem->memory }}</td>
+                  <td>{{ $crudstestsystem->system }}</td>
+                  <td>{{ $crudstestsystem->value }}</td>
+                  <td>
+                      <a href="virtual/visualizar/{{ $crudstestsystem->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      <a href="virtual/editar/{{ $crudstestsystem->id }}"><i class="material-icons">edit</i></a>
+                      <a href="virtual/apagar/{{ $crudstestsystem->id }}"><i class="material-icons">delete_forever</i></a>
+                  </td>
+                </tr>
+              @endforeach              
+            </tbody>
+          </table>
+        </div>
+        <!-- datatable ends -->
+      </div>
+    </div>
+  </div>
+</section>
+<!-- users list ends -->
+@endsection
+
+{{-- vendor scripts --}}
+@section('vendor-script')
+<script src="{{asset('vendors/data-tables/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('vendors/data-tables/extensions/responsive/js/dataTables.responsive.min.js')}}"></script>
+@endsection
+
+{{-- page script --}}
+@section('page-script')
+<script src="{{asset('js/scripts/page-users.js')}}"></script>
+@endsection
