d5dfb4d889ba02e8ff8e851cc62c8439f61783ff
bruninhomf <bruninhomf@msn.com>
2020-12-03 17:30:51 -0300

    Adicionando função para inserir um módulo pro vez no banco de dados

--- app/RequirementTestSystem.php
+++ app/RequirementTestSystem.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use App\RequirementTestModule;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,16 @@ class RequirementTestSystem extends Model
     protected $fillable = [
         'name_system', 
     ];
+    public function inserirmodulo($dados) {
+        if($dados && isset($dados['modulos'])) {
+            foreach($dados['modulos'] as $modulo) {
+                RequirementTestModule::create([
+                   'system_id' => $this->id,
+                   'name'  => $modulo['name'],
+                   'description'  => $modulo['description'],
+                   'status' => $modulo['status'],
+                ]);
+            }
+        }
+    }
 }
