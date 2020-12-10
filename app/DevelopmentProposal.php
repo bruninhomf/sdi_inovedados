4b5a17329221e636283aca755ade2841dfd5873a
bruninhomf <bruninhomf@msn.com>
2020-12-10 09:40:46 -0300

    Adicionando um parametro a model

--- app/DevelopmentProposal.php
+++ app/DevelopmentProposal.php
@@ -8,9 +8,9 @@ class DevelopmentProposal extends Model
 {
     protected $fillable = [
         'project', 'version', 'date', 'business_solution_one', 'business_solution_two', 
-        'business_solution_three', 'requirements', 'start_development', 
-        'texting_release', 'start_test', 'homologation', 'contact_name', 'client', 
-        'address', 'phone', 'cnpj', 'amount', 'first_payment', 'first_payment_date', 
-        'second_payment', 'second_payment_date', 'proposal_validity',
+        'business_solution_three', 'requirements', 'start_development', 'texting_release', 
+        'start_test', 'homologation', 'delivery', 'contact_name', 'client', 'address', 
+        'phone', 'cnpj', 'amount', 'first_payment', 'first_payment_date', 'second_payment', 
+        'second_payment_date', 'proposal_validity', 
     ];
 }
