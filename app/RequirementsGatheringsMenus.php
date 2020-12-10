b8db6ad8a8cbc5339b951ac6050c90cb2711a6a7
bruninhomf <bruninhomf@msn.com>
2020-12-10 09:44:23 -0300

    Adicionando a model levantamento de requisitos-menus

---
+++ app/RequirementsGatheringsMenus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RequirementsGatheringsMenus extends Model
+{
+    protected $fillable = [
+        'titles_id', 'menu', 
+    ];
+}
