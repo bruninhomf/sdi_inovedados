a8e7d0a656d1a3e9fe2a33f6d6c866e167475564
bruninhomf <bruninhomf@msn.com>
2020-12-14 18:17:39 -0300

    Adicionando Model para testes funcionais

---
+++ app/FunctionalitTestRequirement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FunctionalitTestRequirement extends Model
+{
+    protected $fillable = [
+        'module_id', 'test', 'result', 'status', 
+    ];
+}
