3ba0b86d7c1322d0514d083c88c82b034dfb998a
bruninhomf <bruninhomf@msn.com>
2020-12-14 18:19:15 -0300

    Adicionando Model para modulos de testes funcionais

---
+++ app/UsecaseTestModule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsecaseTestModule extends Model
+{
+    protected $fillable = [
+        'usecase_id', 'description'
+    ];
+
+
+    public function insertRequeriments($dados)
+    {
+        UsecaseTestRequirement::create([
+            'module_id' => $this->id,
+            'test'      => $dados['test'],
+            'result'    => $dados['result'],
+            'status'    => $dados['status']
+        ]);
+    }
+}
