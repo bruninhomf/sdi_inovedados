657f09871fcbb86d0a68bf73f49e1e8daf5f2f51
bruninhomf <bruninhomf@msn.com>
2020-12-14 18:31:54 -0300

    Adicionando informações ao layout da página de proposta virtual

--- resources/views/pages/virtual-proposal-pdf.blade.php
+++ resources/views/pages/virtual-proposal-pdf.blade.php
@@ -19,23 +19,34 @@
           </tr>
           <tr>
             <td style="background-color: #be575e; height: 280px;"></td>
-              <td rowspan="2" style="background-color: #891629; color: white; width: 600px; padding: 0px">
-                <h1 style="font-size: 50px; padding: 0px 0px 0px 50px;">PROPOSTA</h1>
-                <h1 style="font-size: 90px; padding: 0px 0px 0px 50px; font-family: Arial, Helvetica, sans-serif; margin: 0px">TÉCNICA</h1>
-              <div>
-                <h4 style="">Servidor<br>Virtual</h4>
-              </div>
-              <div style="background-color: white; width: 200px; height: 10px;">@</div>
-              <div style="text-align: right">
-                <div>contato@inovedados.com.br <span><i>email</i></span></div>
-                <div>+55 (31) 4042-2999 <span><i>phone</i></span></div>
-                <div>Rua dos Guajajaras, 910, Sala 1619 <span><i>place</i></span></div>
-                <div>inovedados <span><i>phone</i></span></div>
-              </div>
+            <td rowspan="2" style="background-color: #891629; color: white; width: 600px; padding: 40px;">
+              <h1 style="font-size: 50px;">PROPOSTA</h1>
+              <h1 style="font-size: 90px; font-family: Arial, Helvetica, sans-serif;">TÉCNICA</h1>
+            
+              <table>
+                <tr>
+                  <td style="padding: 25px 0px 25px 0px;">
+                    <h2>Servidor Virtual</h2>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="background-color: white; position:absolute; width: 130px; height: 10px;"></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td style="color: white; padding-left: 60px; padding-top: 70px; text-align: right; white-space: nowrap;">
+                    <div><h4>contato@inovedados.com.br</h4> <img src="images/icon/email_white.png" style="width: 12px; margin-left: 7px"></div>
+                    <div><h4>+55 (31) 4042-2999 </h4> <img src="images/icon/phone_white.png" style="width: 7px; margin-left: 6px"></div>
+                    <div><h4>Rua dos Guajajaras, 910, Sala 1619 </h4> <img src="images/icon/address_white.png" style="margin-left: 4px"></div>
+                    <div><h4>inovedados </h4> <img src="images/icon/skype_white.png" style="width: 10px; margin-left: 5px"></div>
+                  </td>
+                </tr>
+              </table>
             </td>
           </tr>
           <tr>
-            <td style="background-color: #262b2c; color: white; font-size: 50px; height: 230px;">2020</td>
+            <td style="background-color: #262b2c; color: white; font-size: 50px; height: 230px; -webkit-transform: rotate(-90.0deg);">{{ date('Y') }}</td>
           </tr>
         </table>
         <div style="padding-top: 20px">          
@@ -43,71 +54,36 @@
           <table style="width: 100%">
             <tbody>
               <tr style="background-color: #f6f6f6;">
-                <td>Processador:</td>
-                <td >{{ $virtualproposal->cpu }}</td>
+                <td style="padding-left: 10px">Processador:</td>
+                <td style="padding-left: 10px">{{ $virtualproposal->cpu }}</td>
               </tr>
               <tr style="background-color: #dbdcdd">
-                <td>Memoria:</td>
-                <td>{{ $virtualproposal->memory }}</td>
+                <td style="padding-left: 10px">Memoria:</td>
+                <td style="padding-left: 10px">{{ $virtualproposal->memory }}</td>
               </tr>
               <tr style="background-color: #f6f6f6">
-                <td>Rede:</td>
-                <td>{{ $virtualproposal->network }}</td>
+                <td style="padding-left: 10px">Rede:</td>
+                <td style="padding-left: 10px">{{ $virtualproposal->network }}</td>
               </tr>
               <tr style="background-color: #dbdcdd">
-                <td>Sistema operacional:</td>
-                <td>{{ $virtualproposal->system }}</td>
+                <td style="padding-left: 10px">Sistema operacional:</td>
+                <td style="padding-left: 10px">{{ $virtualproposal->system }}</td>
               </tr>
-              <tr style="background-color: #dbdcdd">
-                <td>Sistema operacional:</td>
-                <td>{{ $virtualproposal->ip }}</td>
+              <tr style="background-color: #f6f6f6">
+                <td style="padding-left: 10px">IP:</td>
+                <td style="padding-left: 10px">{{ $virtualproposal->ip }}</td>
               </tr>
             </tbody>
           </table>
-        </div>
-
-        <div style="padding-left: 20px">
-          <ul>
-            <li>
-              FTP
-            </li>
-            <li>
-              FTPS
-            </li>
-            <li>
-              SFTP
-            </li>
-            <li>
-              SCP
-            </li>
-            <li>
-              FSamba/CIFSTP
-            </li>
-            <li>
-              BorgBackup
-            </li>
-            <li>
-              rsync via SSH
-            </li>
-            <li>
-              HTTPS
-            </li>
-            <li>
-              WebDAV
-            </li>
-            <li>
-              Useable as network drive
-            </li>
-          </ul>
         </div>
 
         <div style="padding: 25px">
           <p>Localização: Data Center Park Falkenstein, Alemanha.</p>
           <div >
             <h6>
-              Valor: R$ 3.804,00 (três mil, oitocentos e quatro reais) mensais
+              Valor: R$ {{ $virtualproposal->value }}
               <br>
-              Validade da proposta: 15 (quinze) dias
+              Validade da proposta: 15 (quinze) dias a contar de: {{ date('d/m/Y') }}.
             </h6>
           </div>
           <div>
@@ -126,18 +102,16 @@
               </li>
             </ul>
           </div>
-          <table style="padding: 150px 0px 0px 0px">
+          <table style="padding: 365px 0px 0px 0px">
             <tr>
               <td style="padding-left: -20px; text-align: left;">
                 <img src="images/logo/logo_inove.png" alt="Inove Dados" style="width: 120px">
               </td>
-              <td style="padding-left: 280px; text-align: right; white-space: nowrap;">
-                <ul>
-                  <li>contato@inovedados.com.br<i >email</i></li>
-                  <li>+55 (31) 4042-2999 <span><i >phone</i></span></li>
-                  <li>Rua dos Guajajaras, 910, Sala 1619 <span><i >place</i></span></li>
-                  <li>inovedados <span><i class="material-icons red-text">phone</i></span></li>
-                </ul>
+              <td style="padding-left: 280px; text-align: right; white-space: nowrap; border: 1px solid white">
+                <div>contato@inovedados.com.br <img src="images/icon/email.png" style="width: 10px; margin-left: 4px"></div>
+                <div>+55 (31) 4042-2999 <img src="images/icon/fone.png" style="width: 10px; margin-left: 4px"></div>
+                <div style="border: 1px solid white">Rua dos Guajajaras, 910, Sala 1619 <img src="images/icon/end.png" style="width: 10px; margin-left: 4px"></div>
+                <div>inovedados <img src="images/icon/skype.png" style="width: 11px; margin-left: 4px"></div>
               </td>
             </tr>
           </table>          
