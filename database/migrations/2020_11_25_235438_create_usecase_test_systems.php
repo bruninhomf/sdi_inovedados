74bfa55ded9644db3a50020a3efe18378b69cb53
bruninhomf <bruninhomf@msn.com>
2020-12-15 23:55:50 -0300

    Corrigindo parametros

--- database/migrations/2020_11_25_235438_create_usecase_test_systems.php
+++ database/migrations/2020_11_25_235438_create_usecase_test_systems.php
@@ -15,7 +15,7 @@ class CreateUsecaseTestSystems extends Migration
     {
         Schema::create('usecase_test_systems', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('project_name');
             $table->timestamps();
         });
     }
