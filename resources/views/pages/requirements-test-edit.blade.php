e02cb11e18bf389ccd9b6620c3f1f4be63305070
bruninhomf <bruninhomf@msn.com>
2020-12-16 12:09:27 -0300

    Adicionando formulário

--- resources/views/pages/requirements-test-edit.blade.php
+++ resources/views/pages/requirements-test-edit.blade.php
@@ -12,7 +12,7 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/script.js')}}"></script>
+<script src="{{asset('js/custom/button.js')}}"></script>
 {{--  <script src="{{asset('js/jquery-3.0.0.js')}}"></script>  --}}
 @endsection
 
@@ -35,33 +35,118 @@
                 <h4 h4 class="card-title indigo-text pb-5"><strong>Teste - Requisitos</strong></h4>
             </div>
             <div class="col s12" id="account">
-                <form action="/teste-requisitos" method="POST">
+                <form action="/teste-requisitos/{{$requirementtestsystems->id}}" method="POST">
                     @csrf
-                  <div class="row">
-                    <div class="col s12">
-                      <div class="row">
-                        <div class="col s12 input-field">
-                          <input id="name_system" name="name_system" type="text" class="validate" data-error=".errorTxt2">
-                          <label for="name_system">Nome do projeto</label>
-                          <small class="errorTxt2"></small>
+                    <div class="row">
+                      <div class="col s12">
+                        <div class="row">
+                            <div class="col s12 input-field">
+                              <input id="project_name" name="project_name" type="text" class="validate" data-error=".errorTxt2" value="{{ $requirementtestsystems->name_system }}">
+                              <label for="project_name">Nome do projeto</label>
+                              <small class="errorTxt2"></small>
+                            </div>
                         </div>
                       </div>
-                    </div>
-                    <div class="col s12 pl-3">
-                        <div class="row element" id='div_0'>
+                      <div class="col s12 pl-3">
+                          <div class="row element" id='rec_0'>
+                            <div class="col s12 right-align pr-1">
+                              <span class='mt-2 btn waves-effect waves-light green darken-1 add'>Modulo<i class="material-icons">add</i></span>
+                            </div>
+                          </div>
+                      </div>
+
+                      {{--  @foreach($requirementtestsystems as $key => $requirementtestsystem)  --}}
+  
+                      <div class='col s12 input-field' id='"+ nextindex +"'>
+                        <label for='name'>Módulo</label><input id='name' name='modulos["+ nextindex +"][name]' type='text' class='validate' data-error='.errorTxt1' value="{{ $requirementtestmodules->name }}">
+                        <small class='errorTxt1'></small>
+                      </div>   
+                      {{--  @endforeach  --}}
+                        <div class='row element' id='rec_0'>
+                          <div class='col s12 right-align pr-1'>
+                          <span class='mt-2 btn waves-effect waves-light green darken-1 addl'>
+                            Modulo
+                            <i class='material-icons'>add</i>
+                          </span>
+                        </div>
+                      </div>   
+                      <div class='col s12 input-field'>
+                        <label for='description'>Descrição</label>
+                        <textarea id='description' name='modulos["+ nextindex +"][description]' class='materialize-textarea'>{{ $requirementtestrequirements->description }}</textarea>
+                        <small class='errorTxt2'></small>
+                      </div>
+                      @foreach($requirementtestmodules as $key => $requirementtestmodule)
+                      {{ $requirementtestmodules->name }}
+                      @endforeach
+                      <div class='input-field col s12'>
+                        <select class='browser-default' id='status' name='modulos["+ nextindex +"][status]' value="123">
+                          <option selected>{{ $requirementtestrequirements->status }}</option>
+                          <option value='Funcionando'>Funcionando</option>
+                          <option value='Não funciona'>Não funciona</option>
+                          <option value='Em correção'>Em correção</option>
+                        </select>
+                        <label style="margin-top: -25px">Situação</label>
+                      </div>
+                      <div class='col s12 right-align'>
+                        <span id='remove_" + nextindex + "' class='red btn btn-reset remove'>
+                          Cancelar 
+                          <i class='material-icons'>clear</i>
+                        </span>
+                      </div>
+
+
+                    {{--  <div class="col s12 pl-3">
+                        <div class="row element-requirement" id='lr_0'>
                           <div class="col s12 right-align pr-1">
-                            <span class='mb-6 btn waves-effect waves-light green darken-1 add'>Modulo<i class="material-icons">add</i></span>
+                            <span class='btn waves-effect waves-light green darken-1 addl'>Modulo<i class="material-icons">add</i></span>
+                          </div>
+                          <div class='col s12 input-field' id='"+ nextindex +"'>
+                            <label for='name'>Titulo</label>
+                            <input id='title' name='modulos["+ nextindex +"][title]' type='text' class='validate'>
+                            <small class='errorTxt1'></small>
+                          </div>
+                          <div class='col s12 input-field'>
+                            <label for='menu'>Menu</label>
+                            <input id='menu' name='modulos["+ nextindex +"][menu]' type='text' class='col s10 validate'>
+                            <div class='col s2 right-align pr-1 pt-1'>
+                              <span class='mt-2 btn btn-floating waves-effect waves-light green darken-1 mr-4 addl' style='border-radius: 5px; width:50px;'>
+                                <i class='material-icons'>add</i>
+                              </span>
+                              <span id='remove_" + nextindex + "' class='red btn btn-floating btn-reset remove' style='border-radius: 5px; width:50px;'>
+                                <i class='material-icons'>clear</i>
+                              </span>
+                            </div>
+                            <small class='errorTxt2'></small>
+                          </div>
+                          <div class='input-field col s12'>
+                            <label for='description'>Descrição</label>
+                            <input id='description' name='modulos["+ nextindex +"][description]' type='text' class='col s10 validate'>
+                            <div class='col s2 right-align pr-1 pt-1'>
+                              <span class='mt-2 btn btn-floating waves-effect waves-light green darken-1 mr-4 addl' style='border-radius: 5px; width:50px;'>
+                                <i class='material-icons'>add</i>
+                              </span>
+                              <span id='lr_" + nextindex + "' class='red btn btn-floating btn-reset remov' style='border-radius: 5px; width:50px;'>
+                                <i class='material-icons'>clear</i>
+                              </span>
+                            </div>
+                            <small class='errorTxt1'></small>
+                          </div>
+
+                          <div class='col s12 right-align'>
+                            <span id='lr_" + nextindex + "' class='red btn btn-reset remov'>
+                              Cancelar <i class='material-icons'>clear</i>
+                            </span>
                           </div>
                         </div>
-                    </div>
+                    </div>  --}}
                                
                     <div class="col s12 display-flex justify-content-end mt-3">
-                        <button type="submit" class="btn indigo">
-                            Salvar
-                        </button>
-                        <button type="cancel" class="btn btn-light">
-                            Cancelar
-                        </button>
+                      <button type="submit" class="btn indigo">
+                          Salvar
+                      </button>
+                      <button type="cancel" class="btn btn-light">
+                          Cancelar
+                      </button>
                     </div>
                   </div>
                 </form>
