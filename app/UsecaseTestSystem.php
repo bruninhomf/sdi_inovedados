3005c53eae566fd8bb1d6ae6b11c5ab46a2557f5
bruninhomf <bruninhomf@msn.com>
2020-12-16 12:33:53 -0300

    Adicionando função de relacionamento entre as tabelas

--- app/UsecaseTestSystem.php
+++ app/UsecaseTestSystem.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace App;
+use App\UsecaseTestModule;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UsecaseTestSystem extends Model
 {
     protected $fillable = [
-        'nome', 
+        'project_name', 
     ];
+    public function insertModules($dados) {
+        if($dados && isset($dados['modulos'])) {
+            foreach($dados['modulos'] as $modulo) {
+                $usecasetestmodule = UsecaseTestModule::create([
+                   'usecase_id'    =>  $this->id,
+                   'description'   =>  $modulo['description']
+                ]);
+
+                $usecasetestmodule->insertRequeriments($modulo);
+            }
+        }
+    }
 }
