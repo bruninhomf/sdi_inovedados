2e6c1d1ef24aaed8134a874b32452f6baf6213da
bruninhomf <bruninhomf@msn.com>
2020-12-15 23:56:44 -0300

    Corrigindo parametros

--- database/migrations/2020_11_25_235511_create_functionalit_test_systems.php
+++ database/migrations/2020_11_25_235511_create_functionalit_test_systems.php
@@ -15,7 +15,7 @@ class CreateFunctionalitTestSystems extends Migration
     {
         Schema::create('functionalit_test_systems', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('project_name');
             $table->timestamps();
         });
     }
