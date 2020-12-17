6bdabc7b0ad520a1a8dd0ed7048a2be19d72b02f
bruninhomf <bruninhomf@msn.com>
2020-12-17 16:52:11 -0300

    Adicionando função de erança dos modulos

--- app/CrudsTestSystem.php
+++ app/CrudsTestSystem.php
@@ -1,12 +1,26 @@
 <?php
 
 namespace App;
+use App\CrudsTestModule;
 
 use Illuminate\Database\Eloquent\Model;
 
 class CrudsTestSystem extends Model
 {
     protected $fillable = [
-        'nome', 
+        'project_name', 
     ];
+    public function insertModules($dados) {
+        if($dados && isset($dados['modulos'])) {
+            foreach($dados['modulos'] as $modulo) {
+                $crudstestmodule = CrudsTestModule::create([
+                   'cruds_id'    =>  $this->id,
+                   'name'         =>  $modulo['name']
+                   
+                ]);
+
+                $crudstestmodule->insertRequeriments($modulo);
+            }
+        }
+    }
 }
