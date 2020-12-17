6c9154ce6098a25b8c1e0ed3ce1123e1687095c4
bruninhomf <bruninhomf@msn.com>
2020-12-17 16:53:06 -0300

    Adicionando função de erança dos modulos

--- app/FunctionalitTestSystem.php
+++ app/FunctionalitTestSystem.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace App;
+use App\FunctionalitTestModule;
 
 use Illuminate\Database\Eloquent\Model;
 
 class FunctionalitTestSystem extends Model
 {
     protected $fillable = [
-        'nome', 
+        'project_name', 
     ];
+    public function insertModules($dados) {
+        if($dados && isset($dados['modulos'])) {
+            foreach($dados['modulos'] as $modulo) {
+                $functionalittestmodule = FunctionalitTestModule::create([
+                   'functionalit_id'    =>  $this->id,
+                   'description'        =>  $modulo['description']
+                ]);
+
+                $functionalittestmodule->insertRequeriments($modulo);
+            }
+        }
+    }
 }
