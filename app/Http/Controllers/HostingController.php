1023a48cd538fa7c429493ae7062227fa03180b8
bruninhomf <bruninhomf@msn.com>
2020-12-18 01:43:29 -0300

    Adicionando alinhamento ao pdf

--- app/Http/Controllers/HostingController.php
+++ app/Http/Controllers/HostingController.php
@@ -69,12 +69,19 @@ class HostingController extends Controller
 
     public function pdf(HostingProposal $hospedagem)
     {
-        // $css = asset('css/pages/pdf.css');
         $html = view('pages/hosting-proposal-pdf', [
             'hostingproposal' => $hospedagem
         ])->render();
-        $mpdf = new Mpdf();
-        // $mpdf->WriteHTML($css, 1);
+        $mpdf = new Mpdf([
+            'mode' => 'utf-8',
+            'format' => 'A4',
+            'margin_top' => 0,
+            'margin_left' => 0,
+            'margin_right' => 0,
+            'margin_bottom' => 0,
+            'margin_footer' => 0,
+            'mirrorMargins' => true,
+        ]);
         $mpdf->WriteHTML($html);
         $mpdf->Output();
     }
