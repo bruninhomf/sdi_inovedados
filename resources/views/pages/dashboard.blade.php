d0e4dac91ee9ccce26f9255b7c6bd2499d6b4fda
bruninhomf <bruninhomf@msn.com>
2021-01-03 16:12:30 -0300

    Adicionando a exibição dos gráficos

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,6 +6,7 @@
 
 {{-- page styles --}}
 @section('page-style')
+
 <link rel="stylesheet" type="text/css" href="{{asset('css/pages/charts-sparkline.css')}}">
 @endsection
 
@@ -15,7 +16,7 @@
 <div class="section" id="materialize-sparkline">
 <!--Simple Line Chart-->
   <div class="row">
-    <div class="col s12 m6 l4">
+    {{--  <div class="col s12 m6 l4">
       <div class="ct-chart card z-depth-2 border-radius-6">
         <div class="card-content">
           <div class="row">
@@ -24,16 +25,18 @@
             </div>
             <div class="col s6 right-align">
               <p class="mt-4 blue-text"><i class="material-icons dp48 vertical-align-bottom">arrow_upward</i>
-                1896</p>
+                {{ $graficRequirementsTotal }}</p>
             </div>
             <div class="col s12 display-flex">
-               <div class="sample-chart-wrapper"><canvas id="polar-chart" height="200"></canvas></div>
+               <div class="sample-chart-wrapper">
+                 <canvas id="requirement-chart" height="200"></canvas>
+                </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="col s12 m6 l4">
+    </div>  --}}
+    <div class="col s12 m6 l6">
       <div class="ct-chart card z-depth-2 border-radius-6">
         <div class="card-content">
           <div class="row">
@@ -41,17 +44,21 @@
               <h4 class="card-title truncate">Propostas</h4>
             </div>
             <div class="col s6 right-align">
-              <p class="mt-4 green-text"><i class="material-icons dp48 vertical-align-bottom">arrow_upward</i>
-                1896</p>
+              <p class="mt-4 green-text">
+                <i class="material-icons dp48 vertical-align-bottom">arrow_upward</i>
+                {{ $graficMonthlyProposalsTotal }}</p>
+              </p>
             </div>
             <div class="col s12 display-flex">
-               <div class="sample-chart-wrapper"><canvas id="polar-ii" height="200"></canvas></div>
+               <div class="sample-chart-wrapper">
+                 <canvas id="proposal-chart" height="200"></canvas>
+                </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m6 l4">
+    <div class="col s12 m6 l6">
       <div class="ct-chart card z-depth-2 border-radius-6">
         <div class="card-content">
           <div class="row">
@@ -59,11 +66,15 @@
               <h4 class="card-title truncate">Testes</h4>
             </div>
             <div class="col s6 right-align">
-              <p class="mt-4 pink-text"><i class="material-icons dp48 vertical-align-bottom">arrow_upward</i>
-                1896</p>
+              <p class="mt-4 pink-text">
+                <i class="material-icons dp48 vertical-align-bottom">arrow_upward</i>
+                {{ $graficMonthlyTestesTotal }}
+              </p>
             </div>
             <div class="col s12 display-flex">
-               <div class="sample-chart-wrapper"><canvas id="polar-iii" height="200"></canvas></div>
+               <div class="sample-chart-wrapper">
+                 <canvas id="test-chart" height="200"></canvas>
+                </div>
             </div>
           </div>
         </div>
@@ -80,7 +91,9 @@
          <h4 class="card-title">Gráfico anual</h4>
          <div class="row">
             <div class="col s12">
-               <div class="sample-chart-wrapper"><canvas id="line-chart" height="400"></canvas></div>
+                <div class="sample-chart-wrapper">
+                  <canvas id="annual-chart" height="400"></canvas>
+                </div>
             </div>
          </div>
       </div>
@@ -98,6 +111,9 @@
 
 {{-- page scripts --}}
 @section('page-script')
+<script>
+  let exportData = @php echo json_encode($exportData); @endphp
+</script>
 <script src="{{asset('js/scripts/charts-chartjs.js')}}"></script>
 <script src="{{asset('js/scripts/charts-sparklines.js')}}"></script>
 @endsection
