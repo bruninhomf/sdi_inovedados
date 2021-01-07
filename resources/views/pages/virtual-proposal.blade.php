a2370e13c1c641e2790d9f44546ed7feb78788dd
bruninhomf <bruninhomf@msn.com>
2021-01-07 17:49:05 -0300

    Adicionando tags de permissão

--- resources/views/pages/virtual-proposal.blade.php
+++ resources/views/pages/virtual-proposal.blade.php
@@ -7,8 +7,7 @@
 {{-- vendors styles --}}
 @section('vendor-style')
 <link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/css/jquery.dataTables.min.css')}}">
-<link rel="stylesheet" type="text/css"
-  href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
 @endsection
 
 {{-- page styles --}}
@@ -24,46 +23,59 @@
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        <div class="row">
+        <div class="row pt-2">
             <div class="col s6">
                 <h4 h4 class="card-title indigo-text pb-5"><strong>Proposta - Servidor Virtual</strong></h4>
             </div>
-            <div class="col s6 right-align">
-                <a href="virtual/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
-            </div>
+            @role('create|admin')
+              <div class="col s6 right-align">
+                  <a href="virtual/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
+              </div>
+            @endrole
         </div>
         <!-- datatable start -->
-        <div class="responsive-table">
+        <div class="responsive-table pt-3">
           <table id="users-list-datatable" class="table">
             <thead>
               <tr>
+                <th></th>
                 <th>ID</th>
                 <th>CPU</th>
                 <th>Memoria</th>
                 <th>Sistema Operacional</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                @role('read|edit|delete|admin')
+                  <th>Ações</th>
+                @endrole
               </tr>
             </thead>
             <tbody>
               @foreach($virtualProposals as $key => $virtualProposal)
                 <tr>
+                  <td></td>
                   <td>{{ $virtualProposal->id }}</td>
                   <td>{{ $virtualProposal->cpu }}</td>
                   <td>{{ $virtualProposal->memory }}</td>
                   <td>{{ $virtualProposal->system }}</td>
                   <td>{{ $virtualProposal->value }}</td>
-                  <td>
-                      <a href="virtual/visualizar/{{ $virtualProposal->id }}"><i class="material-icons">remove_red_eye</i></a>
-                      <a href="virtual/editar/{{ $virtualProposal->id }}"><i class="material-icons">edit</i></a>
-                      <a href="virtual/apagar/{{ $virtualProposal->id }}"><i class="material-icons">delete_forever</i></a>
-                  </td>
+                  @role('read|edit|delete|admin')
+                    <td>
+                      @role('read|admin')
+                        <a href="virtual/visualizar/{{ $virtualProposal->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      @endrole
+                      @role('edit|admin')
+                        <a href="virtual/editar/{{ $virtualProposal->id }}"><i class="material-icons">edit</i></a>
+                      @endrole
+                      @role('delete|admin')
+                        <a href="virtual/apagar/{{ $virtualProposal->id }}"><i class="material-icons">delete_forever</i></a>
+                      @endrole
+                    </td>
+                  @endrole
                 </tr>
               @endforeach              
             </tbody>
           </table>
         </div>
-        <!-- datatable ends -->
       </div>
     </div>
   </div>
@@ -79,5 +91,5 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
+<script src="{{asset('js/scripts/virtual-proposal.js')}}"></script>
 @endsection
