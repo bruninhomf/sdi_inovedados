4efc35543b5baf2745609e244961d81ac8aacefa
bruninhomf <bruninhomf@msn.com>
2021-01-07 17:50:17 -0300

    Adicionando tags de permissão

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -2,13 +2,12 @@
 @extends('layouts.contentLayoutMaster')
 
 {{-- page title --}}
-@section('title','Usuários')
+@section('title','Users list')
 
 {{-- vendors styles --}}
 @section('vendor-style')
 <link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/css/jquery.dataTables.min.css')}}">
-<link rel="stylesheet" type="text/css"
-  href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
 @endsection
 
 {{-- page styles --}}
@@ -20,45 +19,55 @@
 @section('content')
 <!-- users list start -->
 <section class="users-list-wrapper section">
-  
+
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        <div class="row">
-            <div class="col s6">
-                <h4 h4 class="card-title indigo-text pb-5"><strong>Usuários</strong></h4>
-            </div>
+        <div class="row pt-2">
+          <div class="col s6">
+            <h4 class="card-title indigo-text pb-5"><strong>Usuários</strong></h4>
+          </div>
+          @role('create|admin')
             <div class="col s6 right-align">
-                <a href="usuario/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">person_add</i>Adicionar usuário</a>
+              <a href="/usuario/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">person_add</i>Adicionar usuário</a>
             </div>
+          @endrole
         </div>
-        <!-- datatable start -->
-        <div class="responsive-table">
+        <div class="responsive-table pt-3">
           <table id="users-list-datatable" class="table">
             <thead>
               <tr>
                 <th></th>
+                <th>ID</th>
                 <th>Name</th>
                 <th>E-mail</th>
                 <th>Situação</th>
-                <th>Ações</th>
+                @role('read|edit|delete|admin')
+                  <th>Ações</th>
+                @endrole
               </tr>
             </thead>
             <tbody>
-              @foreach ($users as $user)
+              @foreach($users as $user)
                 <tr>
+                  <td></td>
                   <td>{{ $user->id }}</td>
                   <td>{{ $user->name }}</td>
                   <td>{{ $user->email }}</td>
-                  <td><span>
-                      <span>{{ $user->status }}</span>
-                    </span>
-                  </td>
-                  <td>
-                      <a href="usuario/visualizar/{{ $user->id }}"><i class="material-icons">remove_red_eye</i></a>
-                      <a href="usuario/editar/{{ $user->id }}"><i class="material-icons">edit</i></a>
-                      <a href="usuario/apagar/{{ $user->id }}"><i class="material-icons">delete_forever</i></a>
-                  </td>
+                  <td class="green-text">{{ $user->status }}</td>
+                  @role('read|edit|delete|admin')
+                    <td>
+                      @role('read|admin')
+                        <a href="usuario/visualizar/{{ $user->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      @endrole
+                      @role('edit|admin')
+                        <a href="usuario/editar/{{ $user->id }}"><i class="material-icons">edit</i></a>
+                      @endrole
+                      @role('delete|admin')
+                        <a href="usuario/apagar/{{ $user->id }}"><i class="material-icons">delete_forever</i></a>
+                      @endrole
+                    </td>
+                  @endrole
                 </tr>
               @endforeach
             </tbody>
@@ -68,7 +77,6 @@
     </div>
   </div>
 </section>
-<!-- users list ends -->
 @endsection
 
 {{-- vendor scripts --}}
@@ -79,5 +87,5 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
+<script src="{{asset('js/scripts/users.js')}}"></script>
 @endsection
