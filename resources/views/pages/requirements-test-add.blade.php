0a9ad12d6af2aa45f4adf35fbcd17c89b5688afc
bruninhomf <bruninhomf@msn.com>
2021-01-07 18:07:51 -0300

    Estilizando página de criação

--- resources/views/pages/requirements-test-add.blade.php
+++ resources/views/pages/requirements-test-add.blade.php
@@ -2,20 +2,27 @@
 @extends('layouts.contentLayoutMaster')
 
 {{-- page title --}}
-@section('title','Proposta - Hospedagem')
+@section('title','Teste de requisitos')
 
 {{-- vendors styles --}}
 @section('vendor-style')
 <link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/css/jquery.dataTables.min.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('vendors/data-tables/extensions/responsive/css/responsive.dataTables.min.css')}}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 @endsection
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/script.js')}}"></script>
-{{--  <script src="{{asset('js/jquery-3.0.0.js')}}"></script>  --}}
+<script src="{{asset('js/requirement.js')}}"></script>
 @endsection
 
+<style>
+  button:after { 
+    margin-left:86% !important;
+  }
+</style>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 {{-- page styles --}}
 @section('page-style')
 <link rel="stylesheet" type="text/css" href="{{asset('css/pages/page-users.css')}}">
@@ -23,59 +30,54 @@
 
 {{-- page content --}}
 @section('content')
-<!-- users list start -->
 <section class="users-list-wrapper section">
-  
+
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        
-        <div class="row">
-            <div class="col s6">
-                <h4 h4 class="card-title indigo-text pb-5"><strong>Teste - Requisitos</strong></h4>
+
+        <div class="col s12">
+            <h4 h4 class="card-title indigo-text pb-5"><strong>Teste de requisitos</strong></h4>
+        </div>
+        <div class="col s12" id="account">
+          <form action="/teste-requisitos" method="POST">
+              @csrf
+            <div class="row">
+              <div class="col s12 input-field">
+                <input id="project_name" name="project_name" type="text" class="validate" required>
+                <label for="project_name">Nome do projeto</label>
+                @error('project_name')
+                <small class="red-text">{{ $message }}</small>
+                @enderror
+              </div>
             </div>
-            <div class="col s12" id="account">
-                <form action="/teste-requisitos" method="POST">
-                    @csrf
-                  <div class="row">
-                    <div class="col s12">
-                      <div class="row">
-                        <div class="col s12 input-field">
-                          <input id="project_name" name="project_name" type="text" class="validate" required>
-                          <label for="project_name">Nome do projeto</label>
-                          @error('project_name')
-                          <small class="red-text">{{ $message }}</small>
-                          @enderror
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col s12 pl-3">
-                        <div class="row element" id='rec_0'>
-                          <div class="col s12 right-align pr-1">
-                            <span class='mt-2 btn waves-effect waves-light green darken-1 add'>Modulo<i class="material-icons">add</i></span>
-                          </div>
-                        </div>
-                    </div>
-                               
-                    <div class="col s12 display-flex justify-content-end mt-3">
-                        <button type="submit" class="btn indigo">
-                            Salvar
-                        </button>
-                        <button type="cancel" class="btn btn-light">
-                            Cancelar
-                        </button>
-                    </div>
-                  </div>
-                </form>
-                <!-- users edit account form ends -->
+
+            <div class="col s12 pl-3">
+              <div class="col s12 right-align pr-1 pb-3">
+                <span class='mt-2 btn waves-effect waves-light green darken-1 add'>Modulo<i class="material-icons">add</i></span>
+              </div>
             </div>
+
+            <div class="accordion accordion-flush" id="accordionElement">
+            </div>
+
+            <div class="col s12 display-flex justify-content-end mt-3">
+              <button type="submit" class="btn indigo">
+                  Salvar
+              </button>
+              <button type="cancel" class="btn btn-light">
+                  Cancelar
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
 </section>
-<!-- users list ends -->
 @endsection
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 {{-- vendor scripts --}}
 @section('vendor-script')
