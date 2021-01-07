7c3a37d3a52aae6ec64de33e43e826135935e11f
bruninhomf <bruninhomf@msn.com>
2021-01-07 18:03:48 -0300

    Adicionando tags de permissão

--- resources/views/pages/requirements-test.blade.php
+++ resources/views/pages/requirements-test.blade.php
@@ -24,36 +24,50 @@
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        <div class="row">
+        <div class="row pt-2">
             <div class="col s6">
                 <h4 h4 class="card-title indigo-text pb-5"><strong>Teste de Requisitos</strong></h4>
             </div>
-            <div class="col s6 right-align">
-                <a href="teste-requisitos/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
-            </div>
+            @role('create|admin')
+              <div class="col s6 right-align">
+                  <a href="teste-requisitos/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
+              </div>
+            @endrole
         </div>
         <!-- datatable start -->
-        <div class="responsive-table">
+        <div class="responsive-table pt-3">
           <table id="users-list-datatable" class="table">
             <thead>
               <tr>
+                <th></th>
                 <th>ID</th>
                 <th>Nome</th>
                 <th>Data</th>
-                <th>Ações</th>
+                @role('read|edit|delete|admin')
+                  <th>Ações</th>
+                @endrole
               </tr>
             </thead>
             <tbody>
               @foreach($requirementtestsystems as $key => $requirementtestsystem)
                 <tr>
+                  <td></td>
                   <td>{{ $requirementtestsystem->id }}</td>
                   <td>{{ $requirementtestsystem->project_name }}</td>
-                  <td>{{ $requirementtestsystem->created_at }}</td>
-                  <td>
-                      <a href="teste-requisitos/visualizar/{{ $requirementtestsystem->id }}"><i class="material-icons">remove_red_eye</i></a>
-                      <a href="teste-requisitos/editar/{{ $requirementtestsystem->id }}"><i class="material-icons">edit</i></a>
-                      <a href="teste-requisitos/apagar/{{ $requirementtestsystem->id }}"><i class="material-icons">delete_forever</i></a>
-                  </td>
+                  <td>{{ date('d/m/Y', strtotime($requirementtestsystem->created_at)) }}</td>
+                  @role('read|edit|delete|admin')
+                    <td>
+                      @role('read|admin')
+                        <a href="teste-requisitos/visualizar/{{ $requirementtestsystem->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      @endrole
+                      @role('edit|admin')
+                        <a href="teste-requisitos/editar/{{ $requirementtestsystem->id }}"><i class="material-icons">edit</i></a>
+                      @endrole
+                      @role('delete|admin')
+                        <a href="teste-requisitos/apagar/{{ $requirementtestsystem->id }}"><i class="material-icons">delete_forever</i></a>
+                      @endrole
+                    </td>
+                  @endrole
                 </tr>
               @endforeach              
             </tbody>
@@ -75,5 +89,5 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
+<script src="{{asset('js/scripts/requirement-test.js')}}"></script>
 @endsection
