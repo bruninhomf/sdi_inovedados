910f5a70bd29b7436d5bbffd36f60c856c83586f
bruninhomf <bruninhomf@msn.com>
2021-01-07 18:08:40 -0300

    Adicionando tags de permissão

--- resources/views/pages/requirements-gathering.blade.php
+++ resources/views/pages/requirements-gathering.blade.php
@@ -24,38 +24,51 @@
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        <div class="row">
-            <div class="col s6">
-                <h4 h4 class="card-title indigo-text pb-5"><strong>Levantamento de Requisitos</strong></h4>
-            </div>
+        <div class="row pt-2">
+          <div class="col s6">
+            <h4 class="card-title indigo-text pb-5"><strong>Levantamento de Requisitos</strong></h4>
+          </div>
+          @role('create|admin')
             <div class="col s6 right-align">
-                <a href="/requisitos/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
+              <a href="/requisitos/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
             </div>
+          @endrole
         </div>
-        <!-- datatable start -->
-        <div class="responsive-table">
+        <div class="responsive-table pt-3">
           <table id="users-list-datatable" class="table">
             <thead>
               <tr>
+                <th></th>
                 <th>ID</th>
                 <th>Nome do Projeto</th>
                 <th>Versão</th>
                 <th>Data</th>
-                <th>Ações</th>
+                @role('read|edit|delete|admin')
+                  <th>Ações</th>
+                @endrole
               </tr>
             </thead>
             <tbody>
               @foreach($requirementsgatherings as $key => $requirementsgathering)
                 <tr>
+                  <td></td>
                   <td>{{ $requirementsgathering->id }}</td>
                   <td>{{ $requirementsgathering->project_name }}</td>
                   <td>{{ $requirementsgathering->version }}</td>
                   <td>{{ date('d/m/Y', strtotime($requirementsgathering->date)) }}</td>
-                  <td>
-                      <a href="requisitos/visualizar/{{ $requirementsgathering->id }}"><i class="material-icons">remove_red_eye</i></a>
-                      <a href="requisitos/editar/{{ $requirementsgathering->id }}"><i class="material-icons">edit</i></a>
-                      <a href="requisitos/apagar/{{ $requirementsgathering->id }}"><i class="material-icons">delete_forever</i></a>
-                  </td>
+                  @role('read|edit|delete|admin')
+                    <td>
+                      @role('read|admin')
+                        <a href="requisitos/visualizar/{{ $requirementsgathering->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      @endrole
+                      @role('edit|admin')
+                        <a href="requisitos/editar/{{ $requirementsgathering->id }}"><i class="material-icons">edit</i></a>
+                      @endrole
+                      @role('delete|admin')
+                        <a href="requisitos/apagar/{{ $requirementsgathering->id }}"><i class="material-icons">delete_forever</i></a>
+                      @endrole
+                    </td>
+                  @endrole
                 </tr>
               @endforeach              
             </tbody>
@@ -77,5 +90,5 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
+<script src="{{asset('js/scripts/requirement-gathering.js')}}"></script>
 @endsection
