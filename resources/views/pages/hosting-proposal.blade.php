207adbf14383cb2f93684cca26f56c859fc50cf1
bruninhomf <bruninhomf@msn.com>
2021-01-07 18:16:37 -0300

    Adicionando tags de permissão

--- resources/views/pages/hosting-proposal.blade.php
+++ resources/views/pages/hosting-proposal.blade.php
@@ -24,42 +24,56 @@
   <div class="users-list-table">
     <div class="card">
       <div class="card-content">
-        <div class="row">
+        <div class="row pt-2">
             <div class="col s6">
                 <h4 h4 class="card-title indigo-text pb-5"><strong>Proposta - Hospedagem</strong></h4>
             </div>
-            <div class="col s6 right-align">
-                <a href="hospedagem/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
-            </div>
+            @role('create|admin')
+              <div class="col s6 right-align">
+                  <a href="hospedagem/novo" class="waves-effect waves-light btn-small"><i class="material-icons left">receipt</i>Nova Proposta</a>
+              </div>
+            @endrole
         </div>
         <!-- datatable start -->
-        <div class="responsive-table">
+        <div class="responsive-table pt-3">
           <table id="users-list-datatable" class="table">
             <thead>
               <tr>
+                <th></th>
                 <th>ID</th>
                 <th>Espaço em disco</th>
                 <th>Trafego</th>
                 <th>Domínio</th>
                 <th>Subdominio</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                @role('read|edit|delete|admin')
+                  <th>Ações</th>
+                @endrole
               </tr>
             </thead>
             <tbody>
               @foreach ($hostingproposals as $hostingproposal)
                 <tr>
+                  <td></td>
                   <td>{{ $hostingproposal->id }}</td>
                   <td>{{ $hostingproposal->diskspace }}</td>
                   <td>{{ $hostingproposal->traffic }}</td>
                   <td>{{ $hostingproposal->domanins }}</td>
                   <td>{{ $hostingproposal->subdomains }}</td>
                   <td>{{ $hostingproposal->value }}</td>
-                  <td>
-                      <a href="hospedagem/visualizar/{{ $hostingproposal->id }}"><i class="material-icons">remove_red_eye</i></a>
-                      <a href="hospedagem/editar/{{ $hostingproposal->id }}"><i class="material-icons">edit</i></a>
-                      <a href="hospedagem/apagar/{{ $hostingproposal->id }}"><i class="material-icons">delete_forever</i></a>
-                  </td>
+                  @role('read|edit|delete|admin')
+                    <td>
+                      @role('read|admin')
+                        <a href="hospedagem/visualizar/{{ $hostingproposal->id }}"><i class="material-icons">remove_red_eye</i></a>
+                      @endrole
+                      @role('edit|admin')
+                        <a href="hospedagem/editar/{{ $hostingproposal->id }}"><i class="material-icons">edit</i></a>
+                      @endrole
+                      @role('delete|admin')
+                        <a href="hospedagem/apagar/{{ $hostingproposal->id }}"><i class="material-icons">delete_forever</i></a>
+                      @endrole
+                    </td>
+                  @endrole
                 </tr>
               @endforeach
             </tbody>
@@ -81,5 +95,5 @@
 
 {{-- page script --}}
 @section('page-script')
-<script src="{{asset('js/scripts/page-users.js')}}"></script>
+<script src="{{asset('js/scripts/hosting-proposal.js')}}"></script>
 @endsection
