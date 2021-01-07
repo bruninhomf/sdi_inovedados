0b17a9eeda7852601f1d245ad74ad5806312029d
bruninhomf <bruninhomf@msn.com>
2021-01-07 17:55:22 -0300

    Adicionando tags de permissão

--- resources/views/pages/usecase-test-view.blade.php
+++ resources/views/pages/usecase-test-view.blade.php
@@ -29,14 +29,18 @@
         <a href="/teste-caso-de-uso" class="btn-small btn-light-indigo">
           <i class="material-icons mb-5">arrow_back</i>
         </a>
+        @role('print|admin')
         <a href="/teste-caso-de-uso/excel/{{ $usecasetestsystems->id }}" class="btn-small btn-waves-light">
           <i class="material-icons mb-3">file_download</i>
           Excel
         </a>
+        @endrole
+        @role('edit|admin')
         <a href="/teste-caso-de-uso/editar/{{ $usecasetestsystems->id }}" class="btn-small indigo">
           <i class="material-icons mb-5">edit</i>
           Editar
         </a>
+        @endrole
       </div>
     </div>
   </div>
