ec631ef309307d05a63849200afdab464ea0c35a
bruninhomf <bruninhomf@msn.com>
2021-01-08 11:26:53 -0300

    Adicionando hasMany e funções para uso de permissões

--- app/PermissionGroup.php
+++ app/PermissionGroup.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class PermissionGroup extends Model
 {
     protected $fillable = [ 'name' ];
+    
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class);
+    }
+
+    public function toCreate()
+    {
+        return $this->hasOne(Permission::class)->where('name', 'like', '%.create');
+    }
+
+    public function toRead()
+    {
+        return $this->hasOne(Permission::class)->where('name', 'like', '%.read');
+    }
+
+    public function toEdit()
+    {
+        return $this->hasOne(Permission::class)->where('name', 'like', '%.edit');
+    }
+
+    public function toDelete()
+    {
+        return $this->hasOne(Permission::class)->where('name', 'like', '%.delete');
+    }
+
+    public function toAdditional()
+    {
+        return $this->hasOne(Permission::class)->where('name', 'like', '%.additional');
+    }
 }
